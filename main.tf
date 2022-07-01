38a13ae91498718a3c15060e79ba2742592ce758
xgadlnd <Jan.Lindmeier@atruvia.de>
2022-07-01 11:44:33 +0200

    Umstellung auf Module

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 
+provider "fakewebservices" {
+  token = var.provider_token
+}
+
 module "test" {
   source  = "app.terraform.io/team-automation/test/tfe"
-  version = "1.0.0"
+  version = "1.0.1"
   # insert required variables here
-  provider_token = var.provider_token
+  count = 2
 }
